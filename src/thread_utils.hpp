6e7a330612724f1e4c9e1d31bb37081ac96b7259
ISHII 2bit <ishii@buffer-renaiss.com>
2015-11-26 01:38:44 +0900

    add std, add lock_guard()

--- src/thread_utils.hpp
+++ src/thread_utils.hpp
@@ -52,27 +52,27 @@ namespace bbb {
         using process = std::function<void(size_t index, std::mutex &mutex)>;
 
         class manager {
-            const size_t size;
+            const std::size_t size;
             std::vector<std::thread> threads;
             std::mutex mutex;
             multithread::process process;
 
-            std::function<void()> create_process(size_t index) {
+            std::function<void()> create_process(std::size_t index) {
                 return [index, this] { process(index, mutex); };
             }
 
         public:
-            manager(size_t size, multithread::process process)
+            manager(std::size_t size, multithread::process process)
             : size(size)
             , process(process) {
                 threads.reserve(size);
-                for (size_t i = 0; i < size; i++) {
+                for(size_t i = 0; i < size; i++) {
                     threads.push_back(std::thread(create_process(i)));
                 }
             }
 
             void join() {
-                for (auto &thread : threads) {
+                for(auto &thread : threads) {
                     if (thread.joinable()) thread.join();
                 }
             }
@@ -87,6 +87,10 @@ namespace bbb {
 
             void unlock() {
                 mutex.unlock();
+            }
+
+            std::unique_lock<std::mutex> lock_guard() {
+                return std::unique_lock<std::mutex>(mutex);
             }
 
             virtual ~manager() {
