03bc441eea61a8b3268f65c571a587c8df3e23e7
ISHII 2bit <ishii@buffer-renaiss.com>
2015-11-26 22:37:07 +0900

    add function_traits

--- src/type_utils.hpp
+++ src/type_utils.hpp
@@ -19,6 +19,7 @@
 #include "constants.hpp"
 
 #include <type_traits>
+#include <tuple>
 
 namespace bbb {
 	template <typename T>
@@ -64,4 +65,48 @@ namespace bbb {
 		>>,
 		T
 	>>;
+
+	namespace function_traits {
+		template <typename T>
+		struct function_info : public function_info<decltype(&T::operator())> {};
+
+		template <typename class_type, typename ret, typename ... arguments>
+		struct function_info<ret(class_type::*)(arguments ...) const> {
+			static constexpr std::size_t arity = sizeof...(arguments);
+			using result_type = ret;
+			using arguments_types_tuple = std::tuple<arguments ...>;
+			template <std::size_t index>
+			using argument_type = get_type<std::tuple_element<index, arguments_types_tuple>>;
+		};
+
+		template <typename class_type, typename ret, typename ... arguments>
+		struct function_info<ret(class_type::*)(arguments ...)> {
+			static constexpr std::size_t arity = sizeof...(arguments);
+			using result_type = ret;
+			using arguments_types_tuple = std::tuple<arguments ...>;
+			template <std::size_t index>
+			using argument_type = get_type<std::tuple_element<index, arguments_types_tuple>>;
+		};
+
+		template <typename ret, typename ... arguments>
+		struct function_info<ret(*)(arguments ...)> {
+			static constexpr std::size_t arity = sizeof...(arguments);
+			using result_type = ret;
+			using arguments_types_tuple = std::tuple<arguments ...>;
+			template <std::size_t index>
+			using argument_type = get_type<std::tuple_element<index, arguments_types_tuple>>;
+		};
+
+		template<typename T>
+		using result_type = typename function_info<T>::result_type;
+		template<typename T>
+		using arguments_types_tuple = typename function_info<T>::arguments_types_tuple;
+		template<typename T, std::size_t index>
+		using argument_type = typename function_info<T>::template argument_type<index>;
+		template<typename T>
+		struct arity {
+			static constexpr std::size_t value = function_info<T>::arity;
+		};
+	};
+	using namespace function_traits;
 };
