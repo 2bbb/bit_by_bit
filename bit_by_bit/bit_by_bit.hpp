727df7a9e999f0d336660ee782e1236796fbbcda
ISHII 2bit <ishii@buffer-renaiss.com>
2016-01-13 21:32:39 +0900

    update headers

--- bit_by_bit/bit_by_bit.hpp
+++ bit_by_bit/bit_by_bit.hpp
@@ -18,11 +18,21 @@
 
 #include "core/constants.hpp"
 #include "core/type_utils.hpp"
-#include "function/lambda_utils.hpp"
+
 #include "container/container_utils.hpp"
+#include "container/container_delegation.hpp"
+#include "container/iterator_utils.hpp"
 #include "container/reusable_array.hpp"
+
+#include "function/lambda_utils.hpp"
+
 #include "thread/thread_utils.hpp"
+
 #include "math/math_utils.hpp"
+
+#include "misc/dirty.hpp"
 #include "misc/stop_watch.hpp"
 #include "misc/logger.hpp"
 #include "misc/simple_test.hpp"
+
+#include "thread/thread_utils.hpp"
