ad7f525b3f1d89b3f0b739f326ea15ee849da0a6
ISHII 2bit <ishii@buffer-renaiss.com>
2016-05-09 11:17:29 +0900

    update test

--- tests/function/direct_lambda.hpp
+++ tests/function/direct_lambda.hpp
@@ -21,6 +21,15 @@
 
 #include <bbb/function/direct_lambda.hpp>
 
+#include <vector>
+
+//bbb_test_begin_prepare(direct_lambda)
+    struct simple_struct {
+        int get_int() const { return 4; }
+        float get_float() const { return 4.0f; }
+    };
+//bbb_test_end_preprare(direct_lambda)
+
 bbb_test_begin_definition(direct_lambda)
     bbb_assert((2 * (1_ * 1_) - (2_ / 3_))(1, 2, 3) == 2 * 1 * 1 - 2 / 3);
     bbb_assert((2 * (1_ * 1_) - (2_ / 3_))(1, 2, 3.0f) == 2 * 1 * 1 - 2 / 3.0f);
@@ -31,4 +40,37 @@ bbb_test_begin_definition(direct_lambda)
     bbb_assert((sin(1_))(2.0 * M_PI) == std::sin(2.0 * M_PI));
     bbb_assert((cos(1_ + 2 * 2_))(1.0, 2.3f) == std::cos(1.0 + 2 * 2.3f));
     bbb_assert(abs(1_)(-4.3) == std::abs(-4.3));
+
+    bbb_assert((!1_)(false) == true);
+    bbb_assert((!1_)(true) == false);
+    bbb_assert((!(1_ || 2_))(true, false) == false);
+
+    bbb_assert((~1_)(2) == ~2);
+
+    bbb_assert((+1_)(false) == +false);
+    bbb_assert((-1_)(true) == -true);
+
+    {
+        std::vector<int> vec = {1, 2, 3};
+        auto f_subscript = 1_[2_];
+        bbb_assert(f_subscript(vec, 0) == vec[0]);
+        bbb_assert(f_subscript(vec, 1) == vec[1]);
+        bbb_assert(f_subscript(vec, 2) == vec[2]);
+    }
+
+    {
+        int arr[] = {1, 2, 3};
+        auto f_subscript = 1_[2_];
+        bbb_assert(f_subscript(arr, 0) == arr[0]);
+        bbb_assert(f_subscript(arr, 1) == arr[1]);
+        bbb_assert(f_subscript(arr, 2) == arr[2]);
+        bbb_assert(f_subscript(0, arr) == arr[0]);
+        bbb_assert(f_subscript(1, arr) == arr[1]);
+        bbb_assert(f_subscript(2, arr) == arr[2]);
+    }
+
+        // TODO implement member pointer
+//    simple_struct p;
+//    bbb_assert(((1_->*2_)(p, &simple_struct::get_int))() == 4);
+//    bbb_assert(((1_->*2_)(p, &simple_struct::get_float))() == 4.0f);
 bbb_test_end_definition(direct_lambda)
