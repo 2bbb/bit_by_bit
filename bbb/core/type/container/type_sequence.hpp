04eef2fb760693f777ba3219eb68c2131a52e3c1
ISHII 2bit <ishii@buffer-renaiss.com>
2016-11-10 22:03:00 +0900

    update type/container
    add:
    type_sequence::size
    is_sequence()

--- bbb/core/type/container/type_sequence.hpp
+++ bbb/core/type/container/type_sequence.hpp
@@ -26,13 +26,34 @@ namespace bbb {
         struct type_sequence;
 
         template <>
-        struct type_sequence<> {};
+        struct type_sequence<> {
+            static constexpr std::size_t size = 0;
+        };
 
         template <typename type, typename ... types>
         struct type_sequence<type, types ...> {
             using head = type;
             using tails = type_sequence<types ...>;
+            static constexpr std::size_t size = sizeof...(types) + 1;
         };
+
+        namespace detail {
+            template <typename sequence>
+            struct is_sequence : std::false_type {};
+
+            template <typename ... types>
+            struct is_sequence<type_sequence<types ...>> : std::true_type {};
+        }
+
+        template <typename sequence>
+        constexpr bool is_sequence() {
+            return detail::is_sequence<sequence>::value;
+        }
+
+        template <typename sequence, typename std::enable_if<is_sequence<sequence>()>::type * = nullptr>
+        constexpr bool is_null() {
+            return sequence::size == 0;
+        }
     };
 
     using namespace type_sequences;
