f35780ec5ea5f6bbe540ba5c216bd293d8073e68
ISHII 2bit <ishii@buffer-renaiss.com>
2016-11-10 15:02:28 +0900

    WIP. add bbb::bind

---
+++ bbb/function/bind.hpp
@@ -0,0 +1,34 @@
+/* **** **** **** **** **** **** **** **** *
+ *
+ *         _/        _/        _/
+ *        _/_/_/    _/_/_/    _/_/_/
+ *       _/    _/  _/    _/  _/    _/
+ *      _/    _/  _/    _/  _/    _/
+ *     _/_/_/    _/_/_/    _/_/_/
+ *
+ * bit by bit
+ * bbb/function/bind.hpp
+ *
+ * author: ISHII 2bit
+ * mail:   2bit@backspace.tokyo
+ *
+ * **** **** **** **** **** **** **** **** */
+
+#pragma once
+
+#include <bbb/core.hpp>
+#include <bbb/function/direct_lambda.hpp>
+
+namespace bbb {
+    namespace binder {
+
+        template <typename object, typename result, typename ... arguments, typename bind_arguments>
+        auto bind(result(object::*meth)(arguments ...), object obj, bind_arguments ...)
+        -> std::function<result(arguments ...)>
+        {
+            return std::bind(meth, obj, )
+        };
+
+    };
+    using namespace binder;
+};
