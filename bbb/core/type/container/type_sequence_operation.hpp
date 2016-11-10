f7f169c4676e5f1d292c570de00639fe737560dd
ISHII 2bit <ishii@buffer-renaiss.com>
2016-11-10 23:48:59 +0900

    add make_direct_product

--- bbb/core/type/container/type_sequence_operation.hpp
+++ bbb/core/type/container/type_sequence_operation.hpp
@@ -372,6 +372,35 @@ namespace bbb {
             >;
         };
 #endif
+        template <typename t, typename u>
+        struct make_direct_product;
+
+        template <typename t, typename u>
+        using make_direct_product_t = get_type<make_direct_product<t, u>>;
+
+        template <typename ... ts, typename ... us>
+        struct make_direct_product<type_sequence<ts ...>, type_sequence<us ...>> {
+            template <typename t>
+            using make_pair = type_sequence<type_sequence<t, us> ...>;
+
+            using type = reduce_sequence_t<concat_sequence_t, map_t<make_pair, ts ...>>;
+        };
+
+#if BBB_EXEC_UNIT_TEST
+        namespace make_direct_product_test {
+            template <typename ... types>
+            using seq = type_sequence<types ...>;
+            using test1 = unit_test::assert<
+                make_direct_product_t<seq<int, char>, seq<float, double>>,
+                seq<seq<int, float>, seq<int, double>, seq<char, float>, seq<char, double>>
+            >;
+
+            using p = seq<int, int, int, int>;
+            using q = seq<char, char, char, char>;
+            static_assert(make_direct_product_t<p, q>::size == p::size * q::size, "");
+            static_assert(make_sequence_unique_t<make_direct_product_t<p, q>>::size == make_sequence_unique_t<p>::size * make_sequence_unique_t<q>::size, "");
+        };
+#endif
     };
 
     using namespace type_sequence_operations;
