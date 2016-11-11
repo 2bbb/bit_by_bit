6fcd8dd3001f424c1906dfa861c3fc91e16bd07a
ISHII 2bit <ishii@buffer-renaiss.com>
2016-11-11 20:15:11 +0900

    implement insert

--- bbb/core/tmp/type_container/type_sequence_operation.hpp
+++ bbb/core/tmp/type_container/type_sequence_operation.hpp
@@ -60,6 +60,45 @@ namespace bbb {
         };
 #endif
 
+        namespace detail {
+            template <typename x, std::size_t index, typename sequence, std::size_t current_index, bool = current_index < index>
+            struct insert;
+            template <typename x, std::size_t index, typename sequence, std::size_t current_index>
+            using insert_t = get_type<insert<x, index, sequence, current_index>>;
+
+            template <typename x, std::size_t index, typename t, typename ... types, std::size_t current_index>
+            struct insert<x, index, type_sequence<t, types ...>, current_index, true> {
+                using type = push_front_t<t, insert_t<x, index, type_sequence<types ...>, current_index + 1>>;
+            };
+            template <typename x, std::size_t index, typename ... types, std::size_t current_index>
+            struct insert<x, index, type_sequence<types ...>, current_index, false> {
+                using type = type_sequence<x, types ...>;
+            };
+        };
+
+        template <typename t, std::size_t index, typename sequence>
+        using insert = detail::insert<t, index, sequence, 0>;
+
+        template <typename t, std::size_t index, typename sequence>
+        using insert_t = get_type<insert<t, index, sequence>>;
+
+#if BBB_EXEC_UNIT_TEST
+        namespace insert_test {
+            using test0 = unit_test::assert<
+                insert_t<int, 0, type_sequence<char, float>>,
+                type_sequence<int, char, float>
+            >;
+            using test1 = unit_test::assert<
+                insert_t<int, 1, type_sequence<char, float>>,
+                type_sequence<char, int, float>
+            >;
+            using test2 = unit_test::assert<
+                insert_t<int, 2, type_sequence<char, float>>,
+                type_sequence<char, float, int>
+            >;
+        };
+#endif
+
         template <typename s, typename t>
         struct concat_sequence;
 
