43d718fcf4ae27ae887c2436808dfb9e42cdb9b7
ISHII 2bit <ishii@buffer-renaiss.com>
2016-11-12 14:06:52 +0900

    add utility, defer

---
+++ bbb/core/tmp/utility.hpp
@@ -0,0 +1,19 @@
+/* **** **** **** **** **** **** **** **** *
+ *
+ *         _/        _/        _/
+ *        _/_/_/    _/_/_/    _/_/_/
+ *       _/    _/  _/    _/  _/    _/
+ *      _/    _/  _/    _/  _/    _/
+ *     _/_/_/    _/_/_/    _/_/_/
+ *
+ * bit by bit
+ * bbb/core/tmp/utility.hpp
+ *
+ * author: ISHII 2bit
+ * mail:   bit_by_bit@2bit.jp
+ *
+ * **** **** **** **** **** **** **** **** */
+
+#pragma once
+
+#include <bbb/core/tmp/utility/defer.hpp>
