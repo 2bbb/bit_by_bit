ed26b4071c17f051247e8aa455e8cf25a1e9d2c4
ISHII 2bit <ishii@buffer-renaiss.com>
2016-11-12 14:31:08 +0900

    change integer_range, rename slice and change arguments
    integer_range
    * exclude `to`
    slice -> slice_variadic
    * from/to to from/length

--- bbb/core/tmp/integer_sequence/integer_range.hpp
+++ bbb/core/tmp/integer_sequence/integer_range.hpp
@@ -17,6 +17,7 @@
 #pragma once
 
 #include <bbb/core/tmp/traits.hpp>
+#include <bbb/core/tmp/utility.hpp>
 #include <bbb/core/tmp/integer_sequence/integer_sequence.hpp>
 #include <bbb/core/tmp/integer_sequence/sequence_operation.hpp>
 
@@ -26,27 +27,37 @@ namespace bbb {
             template <typename integer_type, integer_type from, integer_type to, integer_type offset = 1>
             struct integer_range {
                 using back_sequence = integer_range<integer_type, from + offset, to, offset>;
-                struct wrapper {
-                    using type = sequence_prepend<integer_type, get_type<back_sequence>, from>;
-                };
-                using type = get_type<conditional_t<
+                using type = resolve_t<conditional_t<
                     from + offset <= to,
-                    wrapper,
+                    defer<sequence_prepend<integer_type, get_type<back_sequence>, from>>,
                     back_sequence
                 >>;
             };
 
             template <typename integer_type, integer_type n, integer_type offset>
             struct integer_range<integer_type, n, n, offset> {
-                using type = integer_sequence<integer_type, n>;
+                using type = integer_sequence<integer_type>;
             };
         };
 
         template <typename integer_type, integer_type from, integer_type to, integer_type offset = 1>
-        using integer_range = get_type<detail::integer_range<integer_type, from, to, offset>>;
+        using integer_range = detail::integer_range<integer_type, from, to, offset>;
+        template <typename integer_type, integer_type from, integer_type to, integer_type offset = 1>
+        using integer_range_t = get_type<integer_range<integer_type, from, to, offset>>;
 
         template <std::size_t from, std::size_t to, std::size_t offset = 1>
         using index_range = integer_range<std::size_t, from, to, offset>;
+        template <std::size_t from, std::size_t to, std::size_t offset = 1>
+        using index_range_t = get_type<index_range<from, to, offset>>;
+
+#ifdef BBB_EXEC_UNIT_TEST
+        namespace integer_range_test {
+            using test1 = unit_test::assert<
+                index_range_t<0, 4>,
+                index_sequence<0, 1, 2, 3>
+            >;
+        };
+#endif
     };
 
     using namespace sequence_range;
--- bbb/core/tmp/type_container/type_sequence/operation/slice.hpp
+++ bbb/core/tmp/type_container/type_sequence/operation/slice.hpp
@@ -23,28 +23,28 @@ namespace bbb {
     namespace type_sequence_operations {
         namespace detail {
             template <typename sequence, typename ... types>
-            struct slice;
+            struct slice_variadic;
 
             template <std::size_t ... indices, typename ... types>
-            struct slice<index_sequence<indices ...>, types ...> {
+            struct slice_variadic<index_sequence<indices ...>, types ...> {
                 using type = type_sequence<type_at<indices, types ...> ...>;
             };
         };
 
-        template <std::size_t from, std::size_t to, typename ... types>
-        using slice = detail::slice<index_range<from, to>, types ...>;
+        template <std::size_t from, std::size_t length, typename ... types>
+        using slice_variadic = detail::slice_variadic<index_range_t<from, from + length>, types ...>;
 
-        template <std::size_t from, std::size_t to, typename ... types>
-        using slice_t = get_type<slice<from, to, types ...>>;
+        template <std::size_t from, std::size_t length, typename ... types>
+        using slice_variadic_t = get_type<slice_variadic<from, length, types ...>>;
 
 #if BBB_EXEC_UNIT_TEST
-        namespace slice_test {
+        namespace slice_variadic_test {
             using test1 = unit_test::assert<
-                slice_t<0, 1, int, int, char>,
+                slice_variadic_t<0, 2, int, int, char>,
                 type_sequence<int, int>
             >;
             using test2 = unit_test::assert<
-                slice_t<1, 2, int, int, char>,
+                slice_variadic_t<1, 2, int, int, char>,
                 type_sequence<int, char>
             >;
         }
@@ -55,7 +55,7 @@ namespace bbb {
 
         template <std::size_t from, std::size_t to, typename ... types>
         struct slice_type_sequence<from, to, type_sequence<types ...>> {
-            using type = slice_t<from, to, types ...>;
+            using type = slice_variadic_t<from, to, types ...>;
         };
 
         template <std::size_t from, std::size_t to, typename sequence>
@@ -64,7 +64,7 @@ namespace bbb {
 #if BBB_EXEC_UNIT_TEST
         namespace slice_type_sequence_test {
             using test1 = unit_test::assert<
-                slice_type_sequence_t<0, 1, type_sequence<int, int, char>>,
+                slice_type_sequence_t<0, 2, type_sequence<int, int, char>>,
                 type_sequence<int, int>
             >;
             using test2 = unit_test::assert<
